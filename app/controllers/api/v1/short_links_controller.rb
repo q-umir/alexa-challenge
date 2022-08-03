568c5a3c1179d772f9ea5bca4dc7a1ceca2dbc81
Midory Flores Arana <midoryfa@gmail.com>
2022-08-03 18:24:19 -0500

    show

--- app/controllers/api/v1/short_links_controller.rb
+++ app/controllers/api/v1/short_links_controller.rb
@@ -1,11 +1,25 @@
+
+require 'rest-client'
+
 class Api::V1::ShortLinksController < Api::V1::BaseController
   before_action :set_alexa_ranke, only: [ :show ]
 
   def index
-    @shortLinks = ShortLink.all
+    url = "https://awis.api.alexa.com/api?Action=UrlInfo&ResponseGroup=Rank&Output=json&Url=cluvi.co"
+    response = RestClient::Request.execute(
+      :method => :get,
+      :url => url,
+      :headers => {x_api_key: '8T7SlYd7Tp16a9VPBovC26LXGhosXjeM97CLbUUv'}
+    )
+
+     p results = JSON.parse(response)
+     alexa = results['Awis']['Results']['Result']['Alexa']
+     alexa['TrafficData']['alexa_rank']
+     render json: { rank: 'hola' }
   end
 
   def show
+
   end
 
   private
--- app/models/short_link.rb
+++ app/models/short_link.rb
@@ -1,3 +1,7 @@
 class ShortLink < ApplicationRecord
-
+    validates_presence_of :url
+    validates :url, format: URI::regexp(%w[http https])
+    validates_uniqueness_of :short_code
+    validates_length_of :url, within: 3..255, on: :create, message: "too long"
+    validates_length_of :short_code, within: 3..255, on: :create, message: "too long"
 end
